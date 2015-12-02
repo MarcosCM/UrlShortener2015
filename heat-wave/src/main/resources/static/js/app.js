7d4652def556880397d834671a7b9963e380dba9
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2015-12-02 21:33:38 +0100

    html javascript

--- heat-wave/src/main/resources/static/js/app.js
+++ heat-wave/src/main/resources/static/js/app.js
@@ -22,3 +22,11 @@ $(document).ready(
                 });
             });
     });
+    function personalizar(){
+      if(document.getElementById("checkPersonalizar").checked){
+        document.getElementById("divPersonalizar").style.visibility = "visible";
+s      }
+      else{
+        document.getElementById("divPersonalizar").style.visibility = "hidden";
+      }
+    }
