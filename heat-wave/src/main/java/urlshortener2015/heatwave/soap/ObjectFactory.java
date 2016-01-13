d820f360ad178e57a37c5d16092dc8b5ae1d0139
xarlieskin <carlos_calanda@hotmail.com>
2016-01-13 17:08:25 +0100

    d

--- heat-wave/src/main/java/urlshortener2015/heatwave/soap/ObjectFactory.java
+++ heat-wave/src/main/java/urlshortener2015/heatwave/soap/ObjectFactory.java
@@ -1,11 +1,3 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.8-b130911.1802 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2016.01.12 a las 05:34:10 PM CET 
-//
-
-
 package urlshortener2015.heatwave.soap;
 
 import javax.xml.bind.annotation.XmlRegistry;
--- heat-wave/src/main/java/urlshortener2015/heatwave/soap/package-info.java
+++ heat-wave/src/main/java/urlshortener2015/heatwave/soap/package-info.java
@@ -1,9 +1,2 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.8-b130911.1802 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2016.01.12 a las 05:34:10 PM CET 
-//
-
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://localhost:8081/soap", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package urlshortener2015.heatwave.soap;
